fbaf6e166c2d7185c2c417f822baf03a381a1c63
Laraib Mushtaq <laraib@thegeeksquad.tech>
2022-07-28 14:50:52 +0530

    added com

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App1() {
+  return <h1>App !</h1>;
+}
+
+function ResumeComponent() {
+  return (
+    <h1>Resume ResumeComponent</h1>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hii I Am Laraib</h1>
+    <div className="App1">
+      <header className="App-header1">
       </header>
+      <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<App1 />}
+        />
+        <Route
+          exact
+          path="/:userId"
+          element={<ResumeComponent />}
+        />
+      </Routes>
+    </Router>
     </div>
   );
 }
